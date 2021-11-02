e4a9e34631f014aac0a216ae1635715915f21aba
Lajaynees <lscingram@yahoo.com>
2021-11-01 22:33:33 -0400

    updated routes/added Workout.js-forgot to save

---
+++ models/Workout.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [
+        {
+            type: {
+                type: String,
+                required: "Must include type of exercise"
+            },
+            name: {
+                type: String,
+                required: "Must include name of exercise"
+            },
+            duration: {
+                type: Number,
+                required: "Must include duration of exercise"
+            },
+            weight: Number,
+            reps: Number,
+            sets: Number,
+            distance: Number
+        }
+    ]
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
